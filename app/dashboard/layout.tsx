85d09470f2a78f03896f990d80249a3eafa5a9fb
wkdtmddus <jangsy1207@gmail.com>
2024-09-10 10:40:06 +0900

    Partial Prerendering

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,6 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
+
+export const experimenal_ppr = true;
  
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
